070ad29349a9a270a62c5a6f29e04c92db9cde28
Kimbyul88 <byul88byul@gmail.com>
2024-09-26 23:07:59 +0900

    Feat: Works 마크업90%, 스타일링20% #5

--- src/app/(main)/works/page.js
+++ src/app/(main)/works/page.js
@@ -1,3 +1,42 @@
+"use-client";
+
+import Image from "next/image";
+import styles from "./page.module.css";
+//
+//
+//
 export default function WorksPage() {
-  return <main>works 페이지 입니다</main>;
+  const tabList = ["ALL", "GAME", "UI/UX"];
+  const worksList = Array(12).fill({
+    imgUrl: "/images/Rectangle.jpg",
+    team: "팀 이름",
+    title: "작품 타이틀",
+    desc: "작품에 대한 설명 입니다.",
+  });
+  return (
+    <main className={styles.main}>
+      <nav>
+        <ul>
+          {tabList.map((el, key) => (
+            <li key={key}>
+              <span>{el}</span>
+            </li>
+          ))}
+        </ul>
+        <div className="indicator"></div>
+      </nav>
+      <section>
+        {worksList.map((el, key) => (
+          <figure key={key}>
+            <Image className="image" src={el.imgUrl} alt={el.title} fill />
+            <figcaption>
+              <span>{el.team}</span>
+              <span>{el.title}</span>
+              <span>{el.desc}</span>
+            </figcaption>
+          </figure>
+        ))}
+      </section>
+    </main>
+  );
 }
