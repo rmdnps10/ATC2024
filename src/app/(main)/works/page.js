57a4ad0ba80ee9bc17c18954e8e3a9bc04ce7f8d
Kimbyul88 <byul88byul@gmail.com>
2024-09-27 20:20:57 +0900

    Feat: 그리드 호버 인터랙션 #5

--- src/app/(main)/works/page.js
+++ src/app/(main)/works/page.js
@@ -8,14 +8,17 @@ import styles from "./page.module.css";
 export default function WorksPage() {
   const tabList = ["ALL", "GAME", "UI/UX"];
   const worksList = Array(12).fill({
+    category: tabList[1],
     imgUrl: "/images/Rectangle.jpg",
-    team: "팀 이름",
-    title: "작품 타이틀",
+    team: "team elephant",
+    title: "코끼리를 냉장고에 넣는 23가지 방법",
+    // title: "@OMO_unofficial",
     desc: "작품에 대한 설명 입니다.",
   });
   return (
     <main className={styles.main}>
       <nav>
+        <div className="indicator"></div>
         <ul>
           {tabList.map((el, key) => (
             <li key={key}>
@@ -23,16 +26,23 @@ export default function WorksPage() {
             </li>
           ))}
         </ul>
-        <div className="indicator"></div>
       </nav>
       <section>
         {worksList.map((el, key) => (
           <figure key={key}>
-            <Image className="image" src={el.imgUrl} alt={el.title} fill />
+            <Image
+              className={styles.image}
+              src={el.imgUrl}
+              alt={el.title}
+              fill
+            />
             <figcaption>
-              <span>{el.team}</span>
-              <span>{el.title}</span>
-              <span>{el.desc}</span>
+              <div className={styles.figCategory}>{el.category}</div>
+              <div className={styles.figBox}>
+                <span className={styles.figTeam}>{el.team}</span>
+                <span className={styles.figTitle}>{el.title}</span>
+                <span className={styles.figDesc}>{el.desc}</span>
+              </div>
             </figcaption>
           </figure>
         ))}
