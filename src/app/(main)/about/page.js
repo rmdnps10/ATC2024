60d4d8a3d847326181ff51e0550066bcedfab096
kayunni <gykim0129@gmail.com>
2024-09-30 02:34:49 +0900

    Style: 후원사 무한 스크롤링 구현 #10

--- src/app/(main)/about/page.js
+++ src/app/(main)/about/page.js
@@ -1,10 +1,13 @@
-"use-client"; 
+"use client"; 
+import { useEffect, useRef } from 'react';
 import styles from "./page.module.css";
 import Image from "next/image";
 //
 //
 //
 export default function AboutPage() {
+  const firstRowSponsors = ['axt-logo', 'axt-logo', 'axt-logo', 'axt-logo', 'axt-logo'];
+  const secondRowSponsors = ['axt-logo', 'axt-logo', 'axt-logo', 'axt-logo', 'axt-logo'];
   return (
     <main className={styles.main}>
       <section className={styles.imageContainer}>
@@ -66,6 +69,36 @@ export default function AboutPage() {
           </p>
         </blockquote>
       </section>
+
+      <section className={styles.sponsorContainer}>
+        <div className={`${styles.scrollRow} ${styles.firstRow}`}>
+          {firstRowSponsors.map((sponsor, index) => (
+            <div className={styles.sponsorBox} key={`row1-${index}`}>
+              <Image src={`/logo/${sponsor}.png`} alt={sponsor} width={160} height={90} />
+            </div>
+          ))}
+            {firstRowSponsors.map((sponsor, index) => (
+          <div className={styles.sponsorBox} key={`row1-copy-${index}`}>
+            <Image src={`/logo/${sponsor}.png`} alt={sponsor} width={160} height={90} />
+          </div>
+          ))}
+        </div>
+        <div className={`${styles.scrollRow} ${styles.secondRow}`}>
+          {secondRowSponsors.map((sponsor, index) => (
+            <div className={styles.sponsorBox} key={`row2-${index}`}>
+              <Image src={`/logo/${sponsor}.png`} alt={sponsor} width={160} height={90} />
+            </div>
+          ))}
+          {secondRowSponsors.map((sponsor, index) => (
+            <div className={styles.sponsorBox} key={`row2-copy-${index}`}>
+              <Image src={`/logo/${sponsor}.png`} alt={sponsor} width={160} height={90} />
+            </div>
+          ))}
+        </div>
+
+      </section>
+
+
       <section className={styles.credit}>
         <div>
           <p>Credit</p>
