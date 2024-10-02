59fe8561a36185293145178febf80a91bfb034c3
kayunni <gykim0129@gmail.com>
2024-10-02 18:10:17 +0900

    Style: 레이아웃 정리 #13

--- src/app/(main)/program/page.js
+++ src/app/(main)/program/page.js
@@ -1,7 +1,9 @@
 'use client'
 import React, { useState } from 'react';
 import styles from './page.module.css';
-
+//
+//
+//
 export default function ProgramPage() {
   const [openPrograms, setOpenPrograms] = useState([]);
 
