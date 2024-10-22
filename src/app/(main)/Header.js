a2dd5f33c321898c2f802ad4906046b33124f828
inyoung <rmdnps10@gmail.com>
2024-10-23 03:09:59 +0900

    Feat: favicon, 각종 metadata 설정 #21

--- src/app/(main)/Header.js
+++ src/app/(main)/Header.js
@@ -1,39 +1,49 @@
-"use-client";
-import React from "react";
-import styles from "./Header.module.css";
-import Link from "next/link";
-import Image from "next/image";
+'use-client'
+import React from 'react'
+import styles from './Header.module.css'
+import Link from 'next/link'
+import Image from 'next/image'
 //
 //
 //
 export default function Header() {
   return (
     <header className={styles.header}>
-      <Link href={"/"}>
-        <Image
-          src="/icon/logo/atc-logo-compose.png"
-          alt="2024 atc 공식 로고"
-          width={140}
-          height={80}
-        />
-      </Link>
+      <div className={styles.logo}>
+        <Link href={'/'}>
+          <Image
+            src="/icon/logo/atc-typography.svg"
+            alt="2024 atc 공식 로고"
+            width={80}
+            height={30}
+          />
+          <div className={styles.line}></div>
+          <Image
+            src="/icon/logo/atc-symbol.svg"
+            alt="2024 atc 공식 로고"
+            width={30}
+            height={40}
+          />
+        </Link>
+      </div>
+
       <ul>
         <li>
-          <Link href={"/about"}>About</Link>
+          <Link href={'/about'}>About</Link>
         </li>
         <li>
-          <Link href={"/works"}>Works</Link>
+          <Link href={'/works'}>Works</Link>
         </li>
         <li>
-          <Link href={"/program"}>프로그램</Link>
+          <Link href={'/program'}>Program</Link>
         </li>
         <li>
-          <Link href={"/archive"}>Archive</Link>
+          <Link href={'/archive'}>Archive</Link>
         </li>
         <li>
-          <Link href={"/map"}>Maps</Link>
+          <Link href={'/map'}>Maps</Link>
         </li>
       </ul>
     </header>
-  );
+  )
 }
