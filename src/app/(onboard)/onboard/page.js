9b0935b3e0f3a56c84697defee4e72b96ef20bd1
Giwan Yoon <davidmario519@gmail.com>
2024-10-30 23:34:16 +0900

    Feat: water shader 배경 적용 #11

--- src/app/(onboard)/onboard/page.js
+++ src/app/(onboard)/onboard/page.js
@@ -1,11 +1,15 @@
 // page.js
-"use client";
+'use client'
 
-import styles from "./page.module.css";
-import Scene from "../../../components/onboard/Scene";
+import styles from './page.module.css'
+import Scene from '../../../components/onboard/Scene'
+import Ocean from '../../../components/onboard/Ocean'
 
 export default function OnBoardPage() {
-  return <div className={styles.root}>
-    <Scene />
-  </div>;
+  return (
+    <div className={styles.root}>
+      {/* <Scene /> */}
+      <Ocean />
+    </div>
+  )
 }
