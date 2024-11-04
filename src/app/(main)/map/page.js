8465a0b16cbe22d28fe459d8aad969f5bfac5edd
Kimbyul88 <byul88byul@gmail.com>
2024-11-04 14:26:28 +0900

    Feat: map branch created #26

--- src/app/(main)/map/page.js
+++ src/app/(main)/map/page.js
@@ -1,4 +1,4 @@
 import styles from './page.module.css'
 export default function Home() {
-  return <main className={styles.main}>산돌 적용 테스트</main>
+  return <main className={styles.main}>map 시작!</main>
 }
