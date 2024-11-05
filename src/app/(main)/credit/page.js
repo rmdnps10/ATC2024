13badf4f53c5fdb2cc0ce3df93a9a55ff9ae87a5
inyoung <rmdnps10@gmail.com>
2024-11-05 13:59:26 +0900

    Feat: credit 페이지 팀별 프로필 사진 및 호버 UI 추가 #21

--- src/app/(main)/credit/page.js
+++ src/app/(main)/credit/page.js
@@ -1,9 +1,41 @@
 'use client'
 import Image from 'next/image'
 import styles from './page.module.css'
+import StaffImageList from '@/components/credit/StaffImageList'
 //
 //
 //
+
+const 기타팀 = ['임동준', '강주헌']
+const 기획팀 = [
+  '신우석',
+  '남현지',
+  '박주현',
+  '송명은',
+  '조현우',
+  '홍서원',
+  '황나금'
+]
+const 대외협력팀 = ['임수빈', '김현규', '우소정', '채서연', '안소연']
+const 디자인팀 = ['이현우', '김예찬', '신서윤', '이현서', '최송화', '현지민']
+const 수집팀 = ['금유현', '김서영', '김현지', '이다은', '정가인']
+const 영상팀 = ['김인규', '김서영', '김시리', '오현서', '최윤정']
+const 웹개발팀 = [
+  '정인영',
+  '길민경',
+  '김가윤',
+  '김민균',
+  '김별',
+  '윤기완',
+  '최보현'
+]
+const 작품팀 = ['신채원', '김대희', '김시윤', '이소윤', '장창엽']
+const 전시팀 = ['김지윤', '김준수', '박서영', '서가언', '주민교']
+
+//
+//
+//
+
 export default function CreditPage() {
   return (
     <main className={styles.main}>
@@ -32,15 +64,12 @@ export default function CreditPage() {
               height="28"
             />
             <h1>Creative Director</h1>
-            <div className={styles.imageContainer}>
-              <Image
-                src="/images/artist-profile/기타/크디_임동준.png"
-                width="200"
-                height="280"
-              />
-            </div>
           </div>
-          <div class="image-container"></div>
+          <StaffImageList
+            teamName={'기타팀'}
+            teamType={'기타'}
+            memberList={기타팀}
+          />
         </section>
         <section className={styles.container}>
           <div className={styles.title}>
@@ -50,10 +79,12 @@ export default function CreditPage() {
               height="28"
             />
             <h1>웹개발팀</h1>
-            <div className={styles.imageContainer}></div>
           </div>
-
-          <div class="image-container"></div>
+          <StaffImageList
+            teamName={'웹개발팀'}
+            teamType={'웹'}
+            memberList={웹개발팀}
+          />
         </section>
         <section className={styles.container}>
           <div className={styles.title}>
@@ -64,44 +95,11 @@ export default function CreditPage() {
             />
             <h1>기획팀</h1>
           </div>
-          <div class={styles.imageContainer}>
-            <Image
-              src="/images/artist-profile/기획/기획_신우석.png"
-              width="200"
-              height="280"
-            />
-            <br />
-            <Image
-              src="/images/artist-profile/기획/기획_남현지.png"
-              width="200"
-              height="280"
-            />
-            <Image
-              src="/images/artist-profile/기획/기획_박주현.png"
-              width="200"
-              height="280"
-            />
-            <Image
-              src="/images/artist-profile/기획/기획_송명은.png"
-              width="200"
-              height="280"
-            />
-            <Image
-              src="/images/artist-profile/기획/기획_조현우.png"
-              width="200"
-              height="280"
-            />
-            <Image
-              src="/images/artist-profile/기획/기획_홍서원.png"
-              width="200"
-              height="280"
-            />
-            <Image
-              src="/images/artist-profile/기획/기획_황나금.png"
-              width="200"
-              height="280"
-            />
-          </div>
+          <StaffImageList
+            teamName={'기획팀'}
+            teamType={'기획'}
+            memberList={기획팀}
+          />
         </section>
         <section className={styles.container}>
           <div className={styles.title}>
@@ -112,7 +110,11 @@ export default function CreditPage() {
             />
             <h1>디자인팀</h1>
           </div>
-          <div class="image-container"></div>
+          <StaffImageList
+            teamName={'디자인팀'}
+            teamType={'디자인'}
+            memberList={디자인팀}
+          />
         </section>
         <section className={styles.container}>
           <div className={styles.title}>
@@ -122,15 +124,12 @@ export default function CreditPage() {
               height="28"
             />
             <h1>대외협력팀</h1>
-            <div class={styles.imageContainer}>
-              <Image
-                src="/images/artist-profile/대외협력/대외협력_임수빈.png"
-                width="200"
-                height="280"
-              />
-            </div>
           </div>
-          <div class="image-container"></div>
+          <StaffImageList
+            teamName={'대외협력팀'}
+            teamType={'대외협력'}
+            memberList={대외협력팀}
+          />
         </section>
         <section className={styles.container}>
           <div className={styles.title}>
@@ -141,7 +140,11 @@ export default function CreditPage() {
             />
             <h1>수집팀</h1>
           </div>
-          <div class="image-container"></div>
+          <StaffImageList
+            teamName={'수집팀'}
+            teamType={'수집'}
+            memberList={수집팀}
+          />
         </section>
         <section className={styles.container}>
           <div className={styles.title}>
@@ -152,7 +155,11 @@ export default function CreditPage() {
             />
             <h1>영상팀</h1>
           </div>
-          <div class="image-container"></div>
+          <StaffImageList
+            teamName={'영상팀'}
+            teamType={'영상'}
+            memberList={영상팀}
+          />
         </section>
         <section className={styles.container}>
           <div className={styles.title}>
@@ -163,7 +170,11 @@ export default function CreditPage() {
             />
             <h1>전시팀</h1>
           </div>
-          <div class="image-container"></div>
+          <StaffImageList
+            teamName={'전시팀'}
+            teamType={'전시'}
+            memberList={전시팀}
+          />
         </section>
         <section className={styles.container}>
           <div className={styles.title}>
@@ -174,7 +185,11 @@ export default function CreditPage() {
             />
             <h1>작품팀</h1>
           </div>
-          <div class="image-container"></div>
+          <StaffImageList
+            teamName={'작품팀'}
+            teamType={'작품'}
+            memberList={작품팀}
+          />
         </section>
       </article>
     </main>
