061b7e905084a8daca4c73cfbba87afabf7571a0
inyoung <rmdnps10@gmail.com>
2024-11-08 19:36:57 +0900

    Style: article 요소 구현 #42

--- src/app/(main)/archive/page.js
+++ src/app/(main)/archive/page.js
@@ -1,3 +1,31 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+import Link from 'next/link'
+
 export default function ArchivePage() {
-  return <main></main>;
+  return (
+    <main className={styles.main}>
+      <article>
+        <h1>
+          <Image
+            src={'/images/archive/atc-slogan-black.png'}
+            alt="코끼리를 냉장고에 넣는법"
+            width={402}
+            height={54}
+          />
+          <span>의 작업과정을 소개합니다.</span>
+        </h1>
+        <p>
+          가로 스크롤을 통해 시간 순서대로 2024 ATC가 만들어진 과정을 감상하실
+          수 있습니다. 컨텐츠를 클릭하시면 각 단계를 더 자세하게 만나보실 수
+          있습니다. <br />
+          <br />
+          ATC를 만든 사람들이 궁금하다면?
+        </p>
+        <Link href={'/credit'}>
+          <button>ATC 크레딧</button>
+        </Link>
+      </article>
+    </main>
+  )
 }
