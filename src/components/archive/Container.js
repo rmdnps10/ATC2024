139ac9a012667f98f24c98c56b510693d928877b
inyoung <rmdnps10@gmail.com>
2024-11-09 03:22:46 +0900

    Style: PC archive page markup #42

--- src/components/archive/Container.js
+++ src/components/archive/Container.js
@@ -18,56 +18,86 @@ export default function Container() {
         width={171 * 0.5}
         height={163 * 0.5}
       />
-      <Button
+      <LowButton
         $top={430}
         $left={40}>
         Creative Director 선정
-      </Button>
-      <Button
+      </LowButton>
+      <LowButton
         $top={360}
         $left={300}>
-        Staff 모집
-      </Button>
-      <Button
-        $top={430}
-        $left={40}>
+        STAFF 모집
+      </LowButton>
+      <LowButton
+        $top={390}
+        $left={490}>
         ATC 킥오프
-      </Button>{' '}
-      <Button
-        $top={430}
-        $left={40}>
+      </LowButton>
+      <LowButton
+        $top={320}
+        $left={680}>
         팀별 첫 회의
-      </Button>{' '}
-      <Button
-        $top={430}
-        $left={40}>
+      </LowButton>
+      <LowButton
+        $top={380}
+        $left={880}>
         주제문 확정
-      </Button>{' '}
-      <Button
-        $top={430}
-        $left={40}>
+      </LowButton>
+      <LowButton
+        $top={420}
+        $left={1020}>
         아티스트 모집
-      </Button>{' '}
-      <Button
-        $top={430}
-        $left={40}>
+      </LowButton>{' '}
+      <LowButton
+        $top={350}
+        $left={1190}>
         브랜딩 완성
-      </Button>{' '}
-      <Button
-        $top={430}
-        $left={40}>
+      </LowButton>{' '}
+      <LowButton
+        $top={300}
+        $left={1420}>
         아티스트 교류의 날
-      </Button>{' '}
-      <Button
-        $top={430}
-        $left={40}>
+      </LowButton>{' '}
+      <LowButton
+        $top={320}
+        $left={1720}>
         스태프 프로필 촬영
-      </Button>
-      <Button
-        $top={430}
-        $left={40}>
+      </LowButton>
+      <LowButton
+        $top={240}
+        $left={1860}>
         외부 인사 미팅
-      </Button>
+      </LowButton>
+      <HightButton
+        $top={100}
+        $left={2450}>
+        팀장단 인터뷰
+      </HightButton>
+      <HightButton
+        $top={10}
+        $left={2580}>
+        작품팀 작품 완성
+      </HightButton>
+      <HightButton
+        $top={50}
+        $left={2780}>
+        웹페이지 배포 완성
+      </HightButton>
+      <HightButton
+        $top={-10}
+        $left={2950}>
+        영상팀 영상 완성
+      </HightButton>
+      <HightButton
+        $top={70}
+        $left={3050}>
+        ATC 전시 설치 시작
+      </HightButton>
+      <Refrigerator
+        src={'/images/archive/냉장고.png'}
+        width={270}
+        height={270}
+      />
     </Scrollsection>
   )
 }
@@ -75,29 +105,53 @@ export default function Container() {
 const Scrollsection = styled.section`
   width: 99%;
   margin: 0 auto;
-  overflow: scroll;
+  overflow-x: scroll;
+  overflow-y: visible;
   margin-top: -12rem;
+  padding-top: 50px;
   position: relative;
   z-index: 1;
 `
 
-const Button = styled.button`
-  display: inline-flex;
+const LowButton = styled.button`
   position: absolute;
-  top: ${props => props.$top + 'px'};
-  left: ${props => props.$left + 'px'};
+  top: 50px;
+  left: 0;
+  transform: ${props => `translate(${props.$left}px, ${props.$top}px)`};
+  width: fit-content;
   padding: 1.3rem 2.8rem;
-  justify-content: center;
-  align-items: center;
-  gap: 1rem;
   color: black;
   border-radius: 1.4rem;
   background: #fff;
   border: none;
 `
 
+const HightButton = styled(LowButton)`
+  background: var(
+    --linear,
+    linear-gradient(
+      90deg,
+      #35cffa 0%,
+      #278ffb 20%,
+      #3c79fb 40%,
+      #dd4ffc 60%,
+      #9734fb 80%,
+      #7340fb 100%
+    )
+  );
+  color: white;
+`
+
 const Elephant = styled(Image)`
   position: absolute;
-  top: 14rem;
-  left: 8rem;
+  top: 190px;
+  left: 80px;
+`
+
+const Refrigerator = styled(Image)`
+  position: absolute;
+  top: -2px;
+  left: -50px;
+  transform: translateX(3440px);
+  z-index: 10;
 `
