4da00c0dee2c1487ded921d1d5dfb93086ca2334
inyoung <rmdnps10@gmail.com>
2024-11-09 03:40:39 +0900

    Feat: 연표 버튼 클릭시 해당 위치로 스무스하게 스크롤되며 코끼리 이동 #42

--- src/components/archive/Container.js
+++ src/components/archive/Container.js
@@ -1,13 +1,24 @@
 'use client'
 import Image from 'next/image'
+import { useRef, useState } from 'react'
 import styled from 'styled-components'
 
 export default function Container() {
   const 연표너비 = 5356 * 0.7
   const 연표높이 = 842 * 0.7
 
+  const [position, setPosition] = useState(0)
+  const scrollRef = useRef(null)
+  const handleButtonClick = x => {
+    setPosition(x)
+    scrollRef.current.scrollTo({
+      left: x,
+      behavior: 'smooth'
+    })
+  }
+
   return (
-    <Scrollsection>
+    <Scrollsection ref={scrollRef}>
       <Image
         src="/images/archive/연표.png"
         width={연표너비}
@@ -17,80 +28,96 @@ export default function Container() {
         src={'/images/archive/코끼리.webp'}
         width={171 * 0.5}
         height={163 * 0.5}
+        position={position}
       />
       <LowButton
         $top={430}
-        $left={40}>
+        $left={40}
+        onClick={() => handleButtonClick(40)}>
         Creative Director 선정
       </LowButton>
       <LowButton
         $top={360}
-        $left={300}>
+        $left={300}
+        onClick={() => handleButtonClick(300)}>
         STAFF 모집
       </LowButton>
       <LowButton
         $top={390}
-        $left={490}>
+        $left={490}
+        onClick={() => handleButtonClick(490)}>
         ATC 킥오프
       </LowButton>
       <LowButton
         $top={320}
-        $left={680}>
+        $left={680}
+        onClick={() => handleButtonClick(680)}>
         팀별 첫 회의
       </LowButton>
       <LowButton
         $top={380}
-        $left={880}>
+        $left={880}
+        onClick={() => handleButtonClick(880)}>
         주제문 확정
       </LowButton>
       <LowButton
         $top={420}
-        $left={1020}>
+        $left={1020}
+        onClick={() => handleButtonClick(1020)}>
         아티스트 모집
-      </LowButton>{' '}
+      </LowButton>
       <LowButton
         $top={350}
-        $left={1190}>
+        $left={1190}
+        onClick={() => handleButtonClick(1190)}>
         브랜딩 완성
-      </LowButton>{' '}
+      </LowButton>
       <LowButton
         $top={300}
-        $left={1420}>
+        $left={1420}
+        onClick={() => handleButtonClick(1420)}>
         아티스트 교류의 날
-      </LowButton>{' '}
+      </LowButton>
       <LowButton
         $top={320}
-        $left={1720}>
+        $left={1720}
+        onClick={() => handleButtonClick(1720)}>
         스태프 프로필 촬영
       </LowButton>
       <LowButton
         $top={240}
-        $left={1860}>
+        $left={1860}
+        onClick={() => handleButtonClick(1860)}>
         외부 인사 미팅
       </LowButton>
       <HightButton
         $top={100}
-        $left={2450}>
+        $left={2450}
+        onClick={() => handleButtonClick(2450)}>
         팀장단 인터뷰
       </HightButton>
       <HightButton
         $top={10}
-        $left={2580}>
+        $left={2580}
+        onClick={() => handleButtonClick(2580)}>
         작품팀 작품 완성
       </HightButton>
       <HightButton
         $top={50}
-        $left={2780}>
+        $left={2780}
+        onClick={() => handleButtonClick(2780)}>
         웹페이지 배포 완성
       </HightButton>
       <HightButton
         $top={-10}
-        $left={2950}>
+        $left={2950}
+        onClick={() => handleButtonClick(2950)}>
         영상팀 영상 완성
       </HightButton>
       <HightButton
         $top={70}
-        $left={3050}>
+        $left={3050}
+        onClick={() => handleButtonClick(3050)}>
         ATC 전시 설치 시작
       </HightButton>
       <Refrigerator
@@ -124,6 +151,7 @@ const LowButton = styled.button`
   border-radius: 1.4rem;
   background: #fff;
   border: none;
+  cursor: pointer;
 `
 
 const HightButton = styled(LowButton)`
@@ -145,7 +173,9 @@ const HightButton = styled(LowButton)`
 const Elephant = styled(Image)`
   position: absolute;
   top: 190px;
-  left: 80px;
+  left: 10px;
+  transform: ${props => `translateX(${props.position}px)`};
+  transition: 0.5s ease;
 `
 
 const Refrigerator = styled(Image)`
