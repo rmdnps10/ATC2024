af806fd9c2b7838dd811aa1138a561b9a5940d45
inyoung <rmdnps10@gmail.com>
2024-11-17 23:21:54 +0900

    Feat: 배경 모달 이미지 스타일 수정 및 캡션 포맷 변경 #65

--- src/app/(main)/archive/store/archiveStore.js
+++ src/app/(main)/archive/store/archiveStore.js
@@ -3,66 +3,66 @@ import { buttons } from './buttonPosition'
 export const archiveStore = [
   {
     imageUrl: '/images/archive/1.png',
-    caption: buttons[0].text
+    caption: `[${buttons[0].text}]`
   },
   {
     imageUrl: '/images/archive/2.png',
-    caption: buttons[1].text
+    caption: `[${buttons[1].text}]`
   },
   {
     imageUrl: '/images/archive/3.png',
-    caption: buttons[2].text
+    caption: `[${buttons[2].text}]`
   },
   {
     imageUrl: '/images/archive/4.png',
-    caption: buttons[3].text
+    caption: `[${buttons[3].text}]`
   },
   {
     imageUrl: '/images/archive/5.png',
-    caption: buttons[4].text
+    caption: `[${buttons[4].text}]`
   },
   {
     imageUrl: '/images/archive/6.png',
-    caption: buttons[5].text
+    caption: `[${buttons[5].text}]`
   },
   {
     imageUrl: '/images/archive/7.png',
-    caption: buttons[6].text
+    caption: `[${buttons[6].text}]`
   },
   {
     imageUrl: '/images/archive/8.png',
-    caption: buttons[7].text
+    caption: `[${buttons[7].text}]`
   },
   {
     imageUrl: '/images/archive/9.png',
-    caption: buttons[8].text
+    caption: `[${buttons[8].text}]`
   },
   {
     imageUrl: '/images/archive/10.png',
-    caption: buttons[9].text
+    caption: `[${buttons[9].text}]`
   },
   {
     imageUrl: '/images/archive/11.png',
-    caption: buttons[10].text
+    caption: `[${buttons[10].text}]`
   },
   {
     imageUrl: '/images/archive/12.png',
-    caption: buttons[11].text
+    caption: `[${buttons[11].text}]`
   },
   {
     imageUrl: '/images/archive/13.png',
-    caption: buttons[12].text
+    caption: `[${buttons[12].text}]`
   },
   {
     imageUrl: '/images/archive/14.png',
-    caption: buttons[13].text
+    caption: `[${buttons[13].text}]`
   },
   {
     imageUrl: '/images/archive/15.png',
-    caption: buttons[14].text
+    caption: `[${buttons[14].text}]`
   },
   {
     imageUrl: '/images/archive/16.png',
-    caption: buttons[15].text
+    caption: '[코끼리를 냉장고에 넣는 방법.]'
   }
 ]
