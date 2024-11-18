a04a8705b49837177b17516ea2feda7503935ffe
inyoung <rmdnps10@gmail.com>
2024-11-18 21:59:44 +0900

    Feat: 메트릭스 컴포넌트 추가 및 구글 애널리틱스, 마이크로소프트 클라리티 통합 #67

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Pretendard } from './font/Font'
 import '@/style/global.css'
 import Script from 'next/script'
+import Metrices from './metrices'
 //
 //
 //
@@ -14,7 +15,10 @@ export default function RootLayout({ children }) {
         type="module"
         src="https://qns2c88qif.execute-api.ap-northeast-2.amazonaws.com/v1/api/js/drop_fontstream_js/?sid=gAAAAABnEfOOGnfu2yeZbBhu-sOVzV8N3b3FtQVQ_KyZLi9JVqc7Li43-jA13mQwm1hFaVfDg9mzc9qYZtyfZM1WwmwmZcoBnJy24y2MyBFRZ3FZSrQ_6IDFYyztXCtOdt4sWPMkRnWBilAj5QuTjylSQ6L-UFcBdONQRMT-Wjnj0thxqwWeTqoLWIV9ArlQsCwqXRhHewUFs2fswGvV02VnDi2RAqMnzRL1xK7YS_9jkBd1Xn212cFbbO42khICT1RBDMyMefkj"
         charset="utf-8"></Script>
-      <body>{children}</body>
+      <body>
+        {children}
+        <Metrices />
+      </body>
     </html>
   )
 }
